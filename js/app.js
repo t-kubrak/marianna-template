4689c81c2815fb03b7ddd426881634742220f25a
t-kubrak <chikashikaari@gmail.com>
2016-07-17 17:51:44 -0400

    Script cleanup

--- js/app.js
+++ js/app.js
@@ -1,1 +1,1 @@
-"use strict";function _classCallCheck(e,n){if(!(e instanceof n))throw new TypeError("Cannot call a class as a function")}var _createClass=function(){function e(e,n){for(var a=0;a<n.length;a++){var t=n[a];t.enumerable=t.enumerable||!1,t.configurable=!0,"value"in t&&(t.writable=!0),Object.defineProperty(e,t.key,t)}}return function(n,a,t){return a&&e(n.prototype,a),t&&e(n,t),n}}(),a=1,B=function(){function e(){_classCallCheck(this,e)}return _createClass(e,[{key:"say",value:function(){console.log(a)}}]),e}(),b=new B;b.say(),function(){console.log("Hello")}();
+"use strict";
